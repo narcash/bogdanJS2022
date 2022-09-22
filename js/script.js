279baa79b5249d64a54defcc9c7c5074acd22abe
Nariman <mirmanov1@mail.ru>
2022-09-22 16:41:16 +0300

    feat: done variables

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@
 3. camelCase все остальные переменные
    названия переменных должны быть понятными
 
-*/
+
 const objectA = {
    a: 10,
    b: true
@@ -24,3 +24,38 @@ console.log(objectA);
 copyOfA.c = 'abc'
 console.log(copyOfA);
 console.log(objectA);
+
+
+
+
+Динамическая типизация в JS
+
+let a  = 10
+a = true
+a = 'Nar'
+
+function fn() {
+   console.log('hello!');
+}
+fn(); // hello!
+
+fn = 'bye!'
+
+fn() // TypeError: fn is not a function
+
+
+Стрелочная функция
+const fn = () => {
+   console.log('Hello!');
+}
+fn(); // Hello!
+fn = 10 // TypeError: assignment to constant variable
+
+fn();
+   Правила работы с переменными:
+   
+   1. Все переменные объявлять перед их использованием
+   
+   2. Стараться использовать const везде, где это возможно
+
+*/
